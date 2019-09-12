d283b3002626975e09b89d7febb845054c2f04ec
filip.kowalski <filip.kowalski@cognifide.com>
2019-09-12 12:10:13 +0200

    add run button

--- src/components/sidebars/SidebarTests.js
+++ src/components/sidebars/SidebarTests.js
@@ -1,17 +1,23 @@
 import React, {Component} from "react";
+import {saveAs} from "file-saver";
 import SearchTests from "../../containers/search/SearchTests";
 import TestsList from "../../containers/sidebars/TestsList";
-import GenerateSuiteButton from "../../containers/buttons/GenerateSuiteButton";
 import LoadSuiteButton from "../../containers/buttons/LoadSuiteButton";
+import SuiteActionButton from "../../containers/buttons/SuiteActionButton";
 
 class SidebarTests extends Component {
+
+  runSuiteOnAet(file) {
+    alert("SENDING TO AET")
+  }
 
   render () {
     return (
       <div className="sidebar-tests">
         <SearchTests />
         <TestsList />
-        <GenerateSuiteButton />
+        <SuiteActionButton label="RUN SUITE, RUN!" handleFile={this.runSuiteOnAet} />
+        <SuiteActionButton label="DOWNLOAD SUITE" handleFile={saveAs}/>
         <LoadSuiteButton />
       </div>
     )
